e0ae77081199b0cde70ace3ecafab04b7723b0ce
Alexandru Artimon <aalecs@gmail.com>
2016-11-08 18:24:35 +0200

    HDX-4991 - HXL Preview - Bite editing

--- src/app/bites/bites.module.ts
+++ src/app/bites/bites.module.ts
@@ -11,17 +11,21 @@ import {SortablejsModule} from 'angular-sortablejs';
 import {RecipeService} from './shared/recipe.service';
 import {HxlproxyService} from './shared/hxlproxy.service';
 import { CookBookService } from './shared/cook-book.service';
+import { InlineEditComponent } from './shared/inline-edit/inline-edit.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     BitesRoutingModule,
-    SortablejsModule
+    SortablejsModule,
+    FormsModule
   ],
   exports: [
-    BitesComponent
+    BitesComponent,
+    InlineEditComponent
   ],
-  declarations: [BitesComponent, BiteListComponent, BiteComponent, ContentToplineComponent, ContentChartComponent],
+  declarations: [BitesComponent, BiteListComponent, BiteComponent, ContentToplineComponent, ContentChartComponent, InlineEditComponent],
   providers: [BiteService, RecipeService, HxlproxyService, CookBookService]
 })
 export class BitesModule { }
