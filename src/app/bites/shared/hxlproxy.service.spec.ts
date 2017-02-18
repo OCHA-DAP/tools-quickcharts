37d52c74171319670eab04ea02452af01059508b
Alexandru Artimon <aalecs@gmail.com>
2017-02-18 13:43:44 +0200

    HDX-5103 - HXL Preview: implement unit tests

--- src/app/bites/shared/hxlproxy.service.spec.ts
+++ src/app/bites/shared/hxlproxy.service.spec.ts
@@ -2,15 +2,82 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { HxlproxyService } from './hxlproxy.service';
+import { Logger } from 'angular2-logger/core';
+import { HttpModule, Http, Response, ResponseOptions, BaseRequestOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+import { AppConfigService } from '../../shared/app-config.service';
+import { ObservableInput } from 'rxjs/Observable';
 
 describe('Service: Hxlproxy', () => {
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({
-  //     providers: [HxlproxyService]
-  //   });
-  // });
-  //
-  // it('should ...', inject([HxlproxyService], (service: HxlproxyService) => {
-  //   expect(service).toBeTruthy();
-  // }));
+  let mockbackend, service;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpModule],
+      providers: [
+        AppConfigService,
+        HxlproxyService,
+        BaseRequestOptions,
+        MockBackend,
+        Logger,
+        {
+          provide: Http,
+          deps: [MockBackend, BaseRequestOptions],
+          useFactory: (backend, options) => {
+            return new Http(backend, options);
+          }
+        }
+      ]
+    });
+  });
+
+  beforeEach(inject([HxlproxyService, MockBackend], (_service, _mockbackend) => {
+    service = _service;
+    mockbackend = _mockbackend;
+  }));
+
+  it('should ...', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should ... throw error when meta rows missing', () => {
+    let response = [];
+    mockbackend.connections.subscribe(connection => {
+      connection.mockRespond(new Response(new ResponseOptions({body: JSON.stringify(response)})));
+    });
+
+    let proxy: HxlproxyService = service;
+    let errorThrown = false;
+    try {
+      proxy.fetchMetaRows('test.csv').subscribe(row => {
+        expect(function(){}).not.toHaveBeenCalled();
+      });
+    } catch (err) {
+      errorThrown = true;
+      expect(err).toBe('There should be 2 meta rows');
+    }
+    expect(errorThrown).toBeTruthy();
+  });
+
+  it('should ... throw error when meta rows missing', () => {
+    let proxy: HxlproxyService = service;
+    let response = '[' +
+      '["id", "ident", "type", "name", "latitude_deg", "longitude_deg", "elevation_ft", "continent", ' +
+      '"iso_country", "iso_region", "municipality", "scheduled_service", "gps_code", "iata_code", ' +
+      '"local_code", "home_link", "wikipedia_link", "keywords", "score", "last_updated"],' +
+      '["#meta+id", "#meta+code", "#loc+airport+type", "#loc+airport+name", "#geo+lat", "#geo+lon", ' +
+      '"#geo+elevation+ft", "#region+continent+code", "#country+code+iso2", "#adm1+code+iso", ' +
+      '"#loc+municipality+name", "#status+scheduled", "#loc+airport+code+gps", "#loc+airport+code+iata", ' +
+      '"#loc+airport+code+local", "#meta+url+airport", "#meta+url+wikipedia", "#meta+keywords", ' +
+      '"#meta+score", "#date+updated"]' +
+      ']';
+    mockbackend.connections.subscribe(connection => {
+      connection.mockRespond(new Response(new ResponseOptions({body: response})));
+    });
+
+    proxy.fetchMetaRows('test.csv').subscribe(result => {
+      expect(result[0][0]).toBe('id');
+    });
+  });
+
 });
--- src/app/shared/app-config.service.spec.ts
+++ src/app/shared/app-config.service.spec.ts
@@ -2,6 +2,7 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { AppConfigService } from './app-config.service';
+import { Params } from '@angular/router';
 
 describe('Service: AppConfig', () => {
   beforeEach(() => {
@@ -12,5 +13,21 @@ describe('Service: AppConfig', () => {
 
   it('should ...', inject([AppConfigService], (service: AppConfigService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should parse params and be able to retrieve them', inject([AppConfigService], (service: AppConfigService) => {
+    const
+      KEY1 = 'key1',
+      VAL1 = 'val1',
+      KEY2 = 'key2',
+      VAL2 = 'val2';
+
+    let params: Params = {};
+    params[KEY1] = VAL1;
+    params[KEY2] = VAL2;
+
+    service.init(params);
+    expect(service.get(KEY1)).toBe(VAL1);
+    expect(service.get(KEY2)).toEqual(VAL2);
   }));
 });
