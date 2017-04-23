8d8b270987c8f0948635d652f0537488e718d88e
Alexandru Artimon <aalecs@gmail.com>
2017-04-23 10:53:07 +0300

    HDX-5236 - HXL Preview 2: create new content types line/bar/pie charts

--- src/app/bites/bite/content/content-chart/content-chart.component.ts
+++ src/app/bites/bite/content/content-chart/content-chart.component.ts
@@ -14,6 +14,8 @@ export class ContentChartComponent implements OnInit, AfterViewInit {
   bite: ChartBite;
 
   elementRef: ElementRef;
+  maxNumberOfValues = 12;
+
   constructor(elementRef: ElementRef) {
     this.elementRef = elementRef;
   }
@@ -28,17 +30,10 @@ export class ContentChartComponent implements OnInit, AfterViewInit {
   }
 
   private generateOptions() {
-    return {
+    const config = {
       bindto: this.elementRef.nativeElement.children[0],
-      data: {
-        columns: [this.bite.values],
-        type: 'bar'
-      },
-      zoom: {
-        enabled: true,
-        type: 'drag', // can be [drag, scroll]
-        extent: [1.5, 2]
-      },
+      data: {},
+      zoom: {},
       size: {
         height: 225
       },
@@ -60,13 +55,42 @@ export class ContentChartComponent implements OnInit, AfterViewInit {
         }
       }
     };
+
+    let values = this.bite.values;
+
+    if (values.length > 4) {
+      config.data = {
+        columns: [values],
+        type: 'bar'
+      };
+    } else {
+      const pieValues = [];
+      for (let i = 0; i < values.length; i++) {
+        pieValues.push([this.bite.categories[i], values[i]]);
+      }
+      console.log(pieValues);
+
+      config.data = {
+        columns: pieValues,
+        type: 'pie'
+      };
+    }
+
+    if (values.length > this.maxNumberOfValues) {
+      config.zoom = {
+        enabled: true,
+        type: 'drag', // can be [drag, scroll]
+        extent: [1.5, 2]
+      };
+    }
+
+    return config;
   }
 
   render(): void {
-    const numberOfValues = 12;
     const c3_chart = c3.generate(this.generateOptions());
-    if (this.bite.values.length > numberOfValues) {
-      c3_chart.internal.brush.extent([0, numberOfValues]).update();
+    if (this.bite.values.length > this.maxNumberOfValues) {
+      c3_chart.internal.brush.extent([0, this.maxNumberOfValues]).update();
       c3_chart.internal.redrawForBrush();
     }
 
