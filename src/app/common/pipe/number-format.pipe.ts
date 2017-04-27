63e86758d51295bbdf1643a63d4e56107c68a8f6
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2017-04-27 16:00:08 +0300

    HDX-5240 implementing unit formatter

--- src/app/common/pipe/number-format.pipe.ts
+++ src/app/common/pipe/number-format.pipe.ts
@@ -5,11 +5,32 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class NumberFormatPipe implements PipeTransform {
 
-  transform(value: number, format: string): string {
+  transform(value: number, format: string, unit: string): string {
+    const modifiedValue = this.computeValueBasedOnUnit(value, unit);
     if (format) {
-      return value.toLocaleString(format);
+      return modifiedValue.toLocaleString(format);
     }
-    return value + '';
+    return modifiedValue + '';
   }
 
+  private computeValueBasedOnUnit(value: number, unit: string) {
+    if (unit) {
+      let newValue = value;
+      switch (unit) {
+        case 'k':
+          newValue = newValue / 1000.0 ;
+          break;
+        case 'mln':
+          newValue = newValue / 1000000.0 ;
+          break;
+        case 'bln':
+          newValue = newValue / 1000000000.0 ;
+          break;
+      }
+      /* Keep only one decimal value  */
+      return Math.round(newValue * 10.0) / 10.0;
+    }
+
+    return value;
+  }
 }
