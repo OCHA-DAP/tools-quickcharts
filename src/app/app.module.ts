c5e6c14318c1cab1338456cf4f00299e1e3d09ec
Alexandru Artimon <aalecs@gmail.com>
2017-09-05 10:20:49 +0300

    HDX-5509 - [quick charts] Add a "loading" indication on the quick charts header bar while loading the charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { Http, HttpModule, RequestOptions, XHRBackend } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { BitesModule } from './bites/bites.module';
@@ -10,6 +10,7 @@ import { AppConfigService } from './shared/app-config.service';
 import { DomEventsService } from './shared/dom-events.service';
 import { AnalyticsService } from './bites/shared/analytics.service';
 import { LOG_LOGGER_PROVIDERS } from 'angular2-logger/app/core/providers';
+import { HttpService } from './shared/http.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,19 @@ import { LOG_LOGGER_PROVIDERS } from 'angular2-logger/app/core/providers';
     BitesModule,
     HxlBitesRoutingModule
   ],
-  providers: [LOG_LOGGER_PROVIDERS, AppConfigService, AnalyticsService, DomEventsService],
+  providers: [
+    LOG_LOGGER_PROVIDERS,
+    {
+      provide: Http,
+      useFactory: (backend: XHRBackend, options: RequestOptions) => {
+        return new HttpService(backend, options);
+      },
+      deps: [XHRBackend, RequestOptions]
+    },
+    AppConfigService,
+    AnalyticsService,
+    DomEventsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/http.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { Http, XHRBackend, RequestOptions, Request, RequestOptionsArgs, Response } from '@angular/http';
+import 'rxjs/Rx';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+declare const $: any;
+
+@Injectable()
+export class HttpService extends Http {
+  public pendingRequests = 0;
+  public showLoading = false;
+  public loadingChange = new BehaviorSubject(false);
+
+  constructor(backend: XHRBackend, defaultOptions: RequestOptions) {
+    super(backend, defaultOptions);
+  }
+
+  changeShowLoading(value: boolean): void {
+    this.showLoading = value;
+    this.loadingChange.next(value);
+  }
+
+  request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
+    return this.intercept(super.request(url, options));
+  }
+
+  intercept(observable: Observable<Response>): Observable<Response> {
+    console.log('In the intercept routine..');
+    this.turnOnModal();
+    return observable
+      // .catch((err, source) => {
+      //   console.log('Caught error: ' + err);
+      // })
+      .do((res: Response) => {
+        console.log('Response: ' + res);
+      }, (err: any) => {
+        console.log('Caught error: ' + err);
+      })
+      .finally(() => {
+        console.log('Finally.. delaying, though.');
+        this.turnOffModal();
+        // const timer = Observable.timer(500);
+        // timer.subscribe(t => {
+        //   this.turnOffModal();
+        // });
+      });
+  }
+
+  private turnOnModal() {
+    this.pendingRequests++;
+    if (!this.showLoading) {
+      // $('body').spin('modal', '#FFFFFF', 'rgba(51, 51, 51, 0.1)');
+      console.log('Turned on modal');
+    }
+    this.changeShowLoading(true);
+  }
+
+  private turnOffModal() {
+    if (this.pendingRequests <= 0) {
+      if (this.showLoading) {
+        // $('body').spin('modal', '#FFFFFF', 'rgba(51, 51, 51, 0.1)');
+      }
+      this.changeShowLoading(false);
+      console.log('Turned off modal');
+    } else {
+      this.pendingRequests--;
+    }
+  }
+}
