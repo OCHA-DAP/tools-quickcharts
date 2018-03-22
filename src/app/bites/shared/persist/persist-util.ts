37aa4cd04f48882b5d42d99d0d5ba9a198ac158b
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2018-03-22 11:19:16 +0200

    HDX-5798 patching bites for new version

--- src/app/bites/shared/persist/persist-util.ts
+++ src/app/bites/shared/persist/persist-util.ts
@@ -1,8 +1,66 @@
 import { HxlPreviewConfig } from './hxl-preview-config';
-import { Bite } from 'hxl-preview-ng-lib';
+import { Bite, BiteLogicFactory } from 'hxl-preview-ng-lib';
 import { Logger } from 'angular2-logger/core';
 
+abstract class Patcher {
+
+  constructor() {}
+
+  public abstract patch(hxlPreviewConfig: HxlPreviewConfig);
+
+  public abstract newConfigVersion(): number;
+
+}
+
+class From2To3Patcher extends Patcher {
+
+  static readonly NEW_CONFIG_VERSION = 3;
+
+  public newConfigVersion(): number {
+    return From2To3Patcher.NEW_CONFIG_VERSION;
+  }
+
+  public patch(hxlPreviewConfig: HxlPreviewConfig) {
+    if (hxlPreviewConfig.bites) {
+      for (let i = 0; i < hxlPreviewConfig.bites.length; i++) {
+        const bite = hxlPreviewConfig.bites[i];
+        this.patchBite(bite);
+      }
+    }
+
+    hxlPreviewConfig.configVersion = this.newConfigVersion();
+
+    return hxlPreviewConfig;
+  }
+
+  private patchBite(bite: Bite) {
+    const biteLogic = BiteLogicFactory.createBiteLogic(bite);
+    if (!bite.uiProperties) {
+      bite.uiProperties = biteLogic.initUIProperties();
+    }
+    if (!bite.computedProperties) {
+      bite.computedProperties = biteLogic.initComputedProperties();
+    }
+    if (!bite.dataProperties) {
+      bite.dataProperties = biteLogic.initDataProperties();
+    }
+
+    [
+      'title', 'description', // general
+      'showGrid', 'swapAxis', 'color', 'sorting', // charts
+      'unit', 'preText', 'postText', 'numberFormat' // key figures
+    ].forEach( property => {
+      if (bite[property]) {
+        bite.uiProperties[property] = bite[property];
+        delete bite[property];
+      }
+    });
+
+  }
+}
 export class PersisUtil {
+
+  static readonly patchers: Patcher[] = [new From2To3Patcher()];
 
   readonly CONFIG_VERSION = 3;
 
@@ -23,6 +81,14 @@ export class PersisUtil {
 
   public configToBitelist(config: string): Bite[] {
     const configObj: HxlPreviewConfig = JSON.parse(config);
+    if (configObj.configVersion < this.CONFIG_VERSION) {
+      PersisUtil.patchers.forEach(patcher => {
+        if (configObj.configVersion < patcher.newConfigVersion()) {
+          patcher.patch(configObj);
+        }
+      });
+    }
+
     if (configObj.configVersion === this.CONFIG_VERSION) {
       return configObj.bites;
     } else {
