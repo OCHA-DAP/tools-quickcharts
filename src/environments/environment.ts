3581a915f739834b53735a092b5c544d7004b33d
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2018-05-09 11:22:19 +0300

    HDX-5866 refactoring how recipeUrl is decided

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,13 @@
 
 export const environment = {
   production: false,
-  hxlProxy: '/hxlproxy/data.json',
+  // hxlProxy: '/hxlproxy/data.json',
+  hxlProxy: 'https://proxy.hxlstandard.org/data.json',
   maxBites: 6,
   snapService: '/snap',
   googleAnalyticsKey: 'UA-48221887-3',
   prodMixpanelKey: '99035923ee0a67880e6c05ab92b6cbc0', // actually hdx-server project not prod
   testMixpanelKey: '875bfe50f9cb981f4e2817832c83c165',
-  prodHostname: 'data.humdata.org' // used by analytics code to decide which key to use
+  prodHostname: 'data.humdata.org', // used by analytics code to decide which key to use
+  recipeUrl: 'https://raw.githubusercontent.com/OCHA-DAP/hxl-recipes/1.0.1/cookbook-library.json'
 };
