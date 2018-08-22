87b45ee82374c8d07694582bdb850ac77bd6a808
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2018-08-23 01:43:40 +0300

    Fix for tools env issue after ng6 upgrade

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,6 +11,6 @@ export const environment = {
   googleAnalyticsKey: 'UA-48221887-3',
   prodMixpanelKey: '99035923ee0a67880e6c05ab92b6cbc0', // actually hdx-server project not prod
   testMixpanelKey: '875bfe50f9cb981f4e2817832c83c165',
-  prodHostname: 'data.humdata.org', // used by analytics code to decide which key to use
+  prodHostnames: ['data.humdata.org', 'tools.humdata.org'], // used by analytics code to decide which key to use
   recipeUrl: 'https://raw.githubusercontent.com/OCHA-DAP/hxl-recipes/1.0.2/cookbook-library.json'
 };
