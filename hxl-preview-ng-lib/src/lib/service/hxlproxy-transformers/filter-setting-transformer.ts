335fa729a5cef45a63d8f3814d12afd5edb070f5
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2018-12-18 01:45:34 +0200

    For filterWith we want AND between filters

--- hxl-preview-ng-lib/src/lib/service/hxlproxy-transformers/filter-setting-transformer.ts
+++ hxl-preview-ng-lib/src/lib/service/hxlproxy-transformers/filter-setting-transformer.ts
@@ -12,7 +12,14 @@ export class FilterSettingTransformer extends AbstractHxlTransformer {
   buildRecipes(): BasicRecipe[] {
     let recipes: BasicRecipe[] = [];
     if (this.filters.filterWith) {
-      recipes.push(new FilterOperation(this.filters.filterWith, true, this.specialFilterValues).recipe);
+      /**
+       * For filterWith we want AND filters. So we need to chain several FilterOperations together. One for each filter
+       * in filterWith. A FilterOperation supports OR between the LIST of filters that is given as an argument.
+       * So we need to transform the one filter to a list.
+       */
+      this.filters.filterWith.forEach(filter => {
+        recipes.push(new FilterOperation([filter], true, this.specialFilterValues).recipe);
+      });
     }
     if (this.filters.filterWithout) {
       recipes.push(new FilterOperation(this.filters.filterWithout, false, this.specialFilterValues).recipe);
