a27ff576c84176d32d28a20b13d84fe6ae65dd42
Alexandru Artimon <aalecs@gmail.com>
2020-10-19 20:56:52 +0300

    HDX-7146 - HXL Preview - Refactor to template vars so we can build only one instance for both CKAN and Tools

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
-  hxlProxy: '/hxlproxy/data.json',
-  maxBites: 6,
-  snapService: 'https://tools.humdata.org/snap',
-  googleAnalyticsKey: 'UA-48221887-3',
-  prodMixpanelKey: '5cbf12bc9984628fb2c55a49daf32e74',
-  testMixpanelKey: '99035923ee0a67880e6c05ab92b6cbc0',
-  prodHostnames: ['data.humdata.org', 'tools.humdata.org'], // used by analytics code to decide which key to use
-  recipeUrl: 'https://raw.githubusercontent.com/OCHA-DAP/hxl-recipes/1.0.11/cookbook-library.json'
+  hxlProxy: window['env']['hxlProxy'] || '/hxlproxy/data.json',
+  maxBites: window['env']['maxBites'] || 6,
+  snapService: window['env']['snapService'] || 'https://tools.humdata.org/snap',
+  googleAnalyticsKey: window['env']['googleAnalyticsKey'] || 'UA-48221887-3',
+  prodMixpanelKey: window['env']['prodMixpanelKey'] || '5cbf12bc9984628fb2c55a49daf32e74',
+  testMixpanelKey: window['env']['testMixpanelKey'] || '99035923ee0a67880e6c05ab92b6cbc0',
+  // used by analytics code to decide which key to use
+  prodHostnames: window['env']['prodHostnames'] || ['data.humdata.org', 'tools.humdata.org'],
+  recipeUrl: window['env']['recipeUrl'] || 'https://raw.githubusercontent.com/OCHA-DAP/hxl-recipes/1.0.11/cookbook-library.json'
 };
